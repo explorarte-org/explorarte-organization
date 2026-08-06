dfd90746366955d68cf9dc907b6df8be86bd1f6f
Eduardo Vega <77175363+Mireuz13@users.noreply.github.com>
2026-08-06 09:05:09 -0400

    feat(decisiongraph): define ledger port

---
+++ internal/decisiongraph/ports.go
@@ -0,0 +1,27 @@
+package decisiongraph
+
+import (
+	"context"
+	"time"
+)
+
+type Ledger interface {
+	CreateRun(context.Context, CreateRunRequest, time.Time) (Run, error)
+	AppendGraph(context.Context, AppendGraphRequest) (GraphVersion, error)
+	StartRun(context.Context, int64, time.Time) error
+	ClaimReadyNode(context.Context, ClaimNodeRequest, time.Time) (NodeClaim, error)
+	FinishExecution(context.Context, FinishExecutionRequest, time.Time) error
+	RecordObservation(context.Context, ObservationRecord, time.Time) error
+	RecordVerification(context.Context, VerificationRecord, time.Time) error
+	RecordTerminalDecision(context.Context, TerminalDecisionRequest, time.Time) error
+	RecoverExpiredExecutions(context.Context, int, time.Time) (int, error)
+	TraceRef(context.Context, int64) (TraceRef, error)
+}
+
+type Clock interface {
+	Now() time.Time
+}
+
+type SystemClock struct{}
+
+func (SystemClock) Now() time.Time { return time.Now().UTC() }
